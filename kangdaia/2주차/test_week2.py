0747602fd67f2f655c99c43f6bd99f61525c2280
Dahye Kang <Dahye.Kang@ibm.com>
2023-09-26 17:13:12 +0900

    [PGS] 여행 경로 / level3 / ~분

---
+++ kangdaia/2주차/test_week2.py
@@ -0,0 +1,57 @@
+from unittest import TestCase, main
+from flight_route import flight_route_backtracking
+from sequence_op import sequence_add_operator
+
+
+class Week2(TestCase):
+    def test_flight_route_1(self):
+        self.assertEqual(
+            flight_route_backtracking(
+                [["ICN", "JFK"],
+                 ["HND", "IAD"],
+                 ["JFK", "HND"]]
+            ),
+            ["ICN", "JFK", "HND", "IAD"]
+        )
+
+    def test_flight_route_2(self):
+        self.assertEqual(
+            flight_route_backtracking(
+                [["ICN", "SFO"],
+                 ["ICN", "ATL"],
+                 ["SFO", "ATL"],
+                 ["ATL", "ICN"],
+                 ["ATL", "SFO"]]
+            ),
+            ["ICN", "ATL", "ICN", "SFO", "ATL", "SFO"]
+        )
+
+    def test_sequence_op_1(self):
+        self.assertEqual(
+            sequence_add_operator(
+                2, [5, 6], [0, 0, 1, 0]
+            ),
+            [30, 30]
+        )
+
+    def test_sequence_op_2(self):
+        self.assertEqual(
+            sequence_add_operator(
+                3, [3, 4, 5], [1, 0, 1, 0]
+            ),
+            [35, 17]
+        )
+
+    # 최댓값: 1-2÷3+4+5×6
+    # 최솟값: 1+2+3÷4-5×6
+    def test_sequence_op_3(self):
+        self.assertEqual(
+            sequence_add_operator(
+                6, [1, 2, 3, 4, 5, 6], [2, 1, 1, 1]
+            ),
+            [54, -24]
+        )
+
+
+if __name__ == '__main__':
+    main()
