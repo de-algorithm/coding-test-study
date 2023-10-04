1da91f3e37e4b26e1eebc6840b54b09a98f1e9f8
Dahye K <kangdiamond@gmail.com>
2023-10-04 19:07:02 +0900

    [BOJ] 우체국 / GOLD 5 / 20분

--- kangdaia/3주차/test_week3.py
+++ kangdaia/3주차/test_week3.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 from surveillance_cam import surveillance_camera
+from post_office import min_dist_post_office
 
 
 class Week3(TestCase):
@@ -7,6 +8,34 @@ class Week3(TestCase):
         self.assertEqual(
             surveillance_camera(
                 [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
+            ), 2
+        )
+
+    def test_post_office_1(self):
+        self.assertEqual(
+            min_dist_post_office(
+                3, [1, 2, 3], [3, 5, 3]
+            ), 2
+        )
+    
+    def test_post_office_2(self):
+        self.assertEqual(
+            min_dist_post_office(
+                4, [1, 2, 3, 4], [1, 1, 1, 1]
+            ), 2
+        )
+
+    def test_post_office_3(self):
+        self.assertEqual(
+            min_dist_post_office(
+                1, [-1000000000], [1]
+            ), -1000000000
+        )
+
+    def test_post_office_4(self):
+        self.assertEqual(
+            min_dist_post_office(
+                2, [1, 2], [1, 2]
             ), 2
         )
 
