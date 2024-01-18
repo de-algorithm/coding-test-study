f1ab9f676849bfeff886fe4b6a0fd5c38a225622
SEONG_HYEON <70497132+Jeon-peng@users.noreply.github.com>
2024-01-18 21:53:43 +0900

    Create test_문자열_압축.py
    add test case

---
+++ test/1주차/test_문자열_압축.py
@@ -0,0 +1,34 @@
+from 기둥과_보_설치 import solution
+from unittest import TestCase, main
+class Cache(TestCase):
+
+    def test_compress_1(self):
+        self.assertEqual(
+            solution("aabbaccc"
+            ), 7
+        )
+
+    def test_compress_2(self):
+        self.assertEqual(
+            solution("ababcdcdababcdcd"
+            ),9
+        )
+    
+  def test_compress_3(self):
+      self.assertEqual(
+          solution("abcabcdede"
+          ),8
+      )
+  
+  def test_compress_4(self):
+    self.assertEqual(
+        solution("abcabcabcabcdededededede"
+        ),14
+    )
+
+
+
+if __name__ == "__main__":
+    main()
+
+
