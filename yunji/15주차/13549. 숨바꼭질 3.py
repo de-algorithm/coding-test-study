6130b606c668dcaf9d7af1231e0414b195d8bdbe
yunji jo <dbtmxlsk2007@naver.com>
2024-01-22 13:16:26 +0900

    [BOJ] 13549. 숨바꼭질3 / GOLD 5 /

---
+++ yunji/15주차/13549. 숨바꼭질 3.py
@@ -0,0 +1,32 @@
+import sys
+import heapq
+sys.stdin = open("yunji/input.txt", "r")
+
+N, K = map(int, sys.stdin.readline().split())
+
+
+
+'''
+(0 ≤ N, K ≤ 100,000)
+
+위치가 중복?이 되면 안되고 
+이동 방법은 제한없고
+
+visited ->  필요X
+위치를 dict = {"위치": "T"or"F"}??
+일단 list로 해보자. 
+
+
+distance 
+
+
+graph
+인접한 노드 = 3가지의 이동방법
+'''
+def dijkstra():
+    time = []
+
+
+    return 
+
+
