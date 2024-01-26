07ca78ef87af2dc9ddec674c4a2afcb4eb5c2af5
Dia <kangdiamond@gmail.com>
2024-01-26 12:26:41 +0900

    Update test_기둥과_보_설치.py

--- test/1주차/test_기둥과_보_설치.py
+++ test/1주차/test_기둥과_보_설치.py
@@ -1,5 +1,13 @@
 import sys
-sys.path.append('../../')
+import os
+
+# Add the directory containing solution.py to the Python path
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.join(current_dir, os.path.pardir, os.path.pardir)
+solution_dir = os.path.abspath(parent_dir)
+sys.path.insert(0, solution_dir)
+
+
 from week1.기둥과_보_설치 import solution
 from unittest import TestCase, main
 
